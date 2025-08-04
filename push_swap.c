bbe5e3a41126af5f1a15aefc9ec6e30e89382922
Petr Volikov <pvolikov@student.42luxembourg.lu>
2025-08-04 20:19:52 +0200

    abrupt end

---
+++ push_swap.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pvolikov <pvolikov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/04 20:17:33 by pvolikov          #+#    #+#             */
+/*   Updated: 2025/08/04 20:19:29 by pvolikov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdarg.h>
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	// Your code here to handle the push_swap logic
+	return (0);
+}
